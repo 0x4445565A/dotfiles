54e22cec8015d006cff494c4029671e6ba62db84
0x4445565a <brandon@ibreak.systems>
2023-06-27 18:18:50 -0500

    Adding autoread on window focus to prevent outo f date git buffers

--- nvim/custom/init.lua
+++ nvim/custom/init.lua
@@ -1,5 +1,11 @@
 vim.opt.guicursor = ""
 
+vim.o.autoread = true
+vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "CursorHoldI", "FocusGained" }, {
+  command = "if mode() != 'c' | checktime | endif",
+  pattern = { "*" },
+})
+
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
