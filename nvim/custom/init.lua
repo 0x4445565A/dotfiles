6ea09fff97c9f0bc2edc138529bb7b73a44a77b1
0x4445565a <brandon@ibreak.systems>
2023-06-28 11:08:46 -0500

    Go autoformating

--- nvim/custom/init.lua
+++ nvim/custom/init.lua
@@ -6,6 +6,24 @@ vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "CursorHoldI", "FocusGai
   pattern = { "*" },
 })
 
+local format_sync_grp = vim.api.nvim_create_augroup("GoImport", {})
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*.go",
+  callback = function()
+   require('go.format').goimport()
+  end,
+  group = format_sync_grp,
+})
+
+local format_sync_fmt_grp = vim.api.nvim_create_augroup("GoFormat", {})
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*.go",
+  callback = function()
+   require('go.format').gofmt()
+  end,
+  group = format_sync_fmt_grp,
+})
+
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
--- nvim/custom/plugins.lua
+++ nvim/custom/plugins.lua
@@ -39,6 +39,19 @@ local plugins = {
             require('rust-tools').inlay_hints.enable()
         end
     },
+    {
+        "ray-x/go.nvim",
+        dependencies = {  -- optional packages
+            "ray-x/guihua.lua",
+            "nvim-treesitter/nvim-treesitter",
+        },
+        config = function()
+            require("go").setup()
+        end,
+        event = {"CmdlineEnter"},
+        ft = {"go", 'gomod'},
+        build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
+    }
 }
 
 return plugins
